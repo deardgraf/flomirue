ee6a5ed339b5c0c5ccfad714cdbdd94b320684c8
deardgraf <kicilinsky@gmail.com>
2025-11-25 17:18:34 +0200

    began making Store page,  added fonts

---
+++ src/pages/Store.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/Store.css';
+
+function Store() {
+  return (
+    <div className="Store">
+        <div className="line"></div>
+        <div className="storeHeader">
+            <p className="sh1">In the world of Flomirue,</p>
+            <p className="sh2">every thread tells a story
+                         <br />of fleeting moments, captured in the lace and light.
+            </p>
+        </div>
+    </div>
+  );
+}
+
+export default Store;
